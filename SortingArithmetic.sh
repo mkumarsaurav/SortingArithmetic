f661a4f23ca9c16e3719d9222c6f5c9268bd3f58
saurav <kumar00914@gmail.com>
2020-10-20 16:09:44 +0530

    Added statements to take three inputs from user

--- SortingArithmetic.sh
+++ SortingArithmetic.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
 echo "Welcome to Sorting Arithmetic Computaions"
+echo "Taking 3 inputs"
+read -p "Enter first number: " num1
+read -p "Enter second number: " num2
+read -p "Enter third number: " num3
